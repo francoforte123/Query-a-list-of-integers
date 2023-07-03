c18f947c32cfb114b58ce11b10f9c22f72023231
rober <rober@DESKTOP-MN9AKPU>
2023-07-03 12:59:07 +0200

    Aggiungere i file di progetto.

---
+++ Query a list of integers/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> ListOfNumbersRandom=new List<int>();
+Random RandomNumbers=new Random();
+
+for(int i = 0; i < 1001; i++)
+{
+    ListOfNumbersRandom.Add(RandomNumbers.Next(0, 70));
+}
+
+Console.WriteLine("sto stampando i numeri presenti nella lista:");
+
+foreach (int x in ListOfNumbersRandom)
+{
+    Console.WriteLine(x);
+}
+
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("sto stampando i numeri dispari presente nella lista");
+List<int> ListOfNumersOdd = (from number in ListOfNumbersRandom where number % 2 != 0 select number).ToList();
+
+foreach (int x in ListOfNumersOdd)
+{
+    Console.WriteLine(x);
+}
+
+
+
+
+
+
+
